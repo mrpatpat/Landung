9f5d859e77d9b669ececad0d284ba738b8a0c355
mrpatpat <Adrian@Adrian-PC>
2014-12-03 19:36:07 +0100

    finishing action

--- Landung/src/oot/landung/Action.java
+++ Landung/src/oot/landung/Action.java
@@ -4,9 +4,32 @@ import java.util.Vector;
 
 public class Action {
 	
-	private Player actor;
-	private Vector<Integer> moveFrom;
-	private Vector<Integer> moveTo;
-	private Vector<Integer>	setTo;
+	private final Player actor;
+	private final Vector<Integer> moveFrom;
+	private final Vector<Integer> moveTo;
+	private final Vector<Integer> setTo;
 	
+	public Action(Player actor,Vector<Integer> moveFrom, Vector<Integer> moveTo, Vector<Integer> setTo){
+		this.actor = actor;
+		this.moveFrom = moveFrom;
+		this.moveTo = moveTo;
+		this.setTo = setTo;
+	}
+
+	public Player getActor() {
+		return actor;
+	}
+
+	public Vector<Integer> getMoveFrom() {
+		return moveFrom;
+	}
+
+	public Vector<Integer> getMoveTo() {
+		return moveTo;
+	}
+
+	public Vector<Integer> getSetTo() {
+		return setTo;
+	}
+
 }
