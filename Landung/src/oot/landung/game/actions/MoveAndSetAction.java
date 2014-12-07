4f7e493913d6a13e46de37d9ea51e2da0167f745
mrpatpat <Adrian@Adrian-PC>
2014-12-07 17:51:56 +0100

    Javadoc in childclasses of Action

--- Landung/src/oot/landung/game/actions/MoveAndSetAction.java
+++ Landung/src/oot/landung/game/actions/MoveAndSetAction.java
@@ -3,8 +3,27 @@ package oot.landung.game.actions;
 import oot.landung.game.player.Player;
 import oot.landung.game.utils.Vector;
 
+/**
+ * Eine Aktion, die aus ziehen und setzen besteht
+ * @author Landung
+ *
+ */
 public class MoveAndSetAction extends Action{
 
+	/**
+	 * Erstellt eine neue Instanz einer MoveAndSetAktion.
+	 * 
+	 * @param sudo
+	 *            true, wenn Regeln missachtet werden sollen
+	 * @param actor
+	 *            der Spieler, der die Aktion ausführt
+	 * @param moveFrom
+	 *            das Feld von dem gezogen werden soll
+	 * @param moveTo
+	 *            das Feld zu dem gezogen werden soll
+	 * @param setTo
+	 *            das Feld, auf das gesetzt wird
+	 */
 	public MoveAndSetAction(boolean sudo, Player actor,
 			Vector<Integer> moveFrom, Vector<Integer> moveTo,
 			Vector<Integer> setTo) {
--- Landung/src/oot/landung/game/actions/SetAction.java
+++ Landung/src/oot/landung/game/actions/SetAction.java
@@ -4,8 +4,19 @@ import oot.landung.game.board.Board;
 import oot.landung.game.player.Player;
 import oot.landung.game.utils.Vector;
 
+/**
+ * Eine Aktion, die auf nur "setzen" beschränkt ist. Beispiel: Im ersten Zug setzt der Spieler einen Stein, aber zieht keinen.
+ * @author Landung
+ *
+ */
 public class SetAction extends Action {
 
+	/**
+	 * Erstellt eine neue Setzaktion.
+	 * @param sudo true, wenn Regeln missachtet werden sollen 
+	 * @param actor der ausführende Spieler
+	 * @param setTo das Feld, auf das gesetzt werden soll
+	 */
 	public SetAction(boolean sudo, Player actor, Vector<Integer> setTo) {
 		super(sudo, actor, null, null, setTo);
 	}
