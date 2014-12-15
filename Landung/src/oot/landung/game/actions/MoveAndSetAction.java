9804d36d2756f5054334f9edb981ee39e9c73a57
Christopher <Christopher@Chris>
2014-12-16 00:55:37 +0100

    changes

--- Landung/src/oot/landung/game/actions/MoveAndSetAction.java
+++ Landung/src/oot/landung/game/actions/MoveAndSetAction.java
@@ -72,7 +72,7 @@ public class MoveAndSetAction extends Action {
 					player.notifyUnvalidMove("Auf diesem Feld befindet sich kein Stein.");
 				return false;
 			}
-			//sfdss
+		
 			// Spieler darf nur eigene Steine bewegen
 			if (moveFrom != null && moveFrom.getOwner() != player) {
 				if (print)
@@ -80,19 +80,7 @@ public class MoveAndSetAction extends Action {
 				return false;
 			}
 
-			// Spieler darf nur auf leere Felder setzen
-			if (setTo != null) {
-				if (print)
-					player.notifyUnvalidMove("Das Feld auf das man setzt muss leer sein.");
-				return false;
-			}
-
-			// Spieler darf nur auf leere Felder ziehen
-			if (getMoveTo() != null && moveTo != null) {
-				if (print)
-					player.notifyUnvalidMove("Man darf nur auf leere Felder ziehen.");
-				return false;
-			}
+	
 
 			// nur in die Richtungen horizontal, vertikal, diagonal ziehen und
 			// mindestens 2 Felder weiter.
@@ -138,6 +126,20 @@ public class MoveAndSetAction extends Action {
 					return false;
 				}
 
+			}
+			
+			// Spieler darf nur auf leere Felder setzen
+			if (setTo != null) {
+				if (print)
+					player.notifyUnvalidMove("Das Feld auf das man setzt muss leer sein.");
+				return false;
+			}
+
+			// Spieler darf nur auf leere Felder ziehen
+			if (getMoveTo() != null && moveTo != null) {
+				if (print)
+					player.notifyUnvalidMove("Man darf nur auf leere Felder ziehen.");
+				return false;
 			}
 
 		}
