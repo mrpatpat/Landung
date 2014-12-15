e523763fe449bfcb1b07c007fca49baf49dd4699
Christopher <Christopher@Chris>
2014-12-16 00:11:56 +0100

    Added stupidai

---
+++ Landung/src/oot/landung/game/player/ai/StupidAi.java
@@ -0,0 +1,33 @@
+package oot.landung.game.player.ai;
+
+import java.util.List;
+
+import oot.landung.game.actions.Action;
+import oot.landung.game.actions.MoveAndSetAction;
+import oot.landung.game.actions.RemoveAction;
+import oot.landung.game.actions.SetAction;
+import oot.landung.game.board.Board;
+import oot.landung.game.player.Player;
+import oot.landung.game.utils.Vector;
+
+/**
+ * RIP PETER
+ * @author Adrian
+ *
+ */
+public class StupidAi implements AiInterface {
+
+	
+	@Override
+	public Action getNextAction(Board board, List<Action> allPossibleActions,
+			int turn) {
+	
+		
+		
+		
+		return allPossibleActions.get((int)(Math.random()*allPossibleActions.size()));
+	}
+
+
+
+}
