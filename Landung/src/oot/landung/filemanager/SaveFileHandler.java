29c07822f817fe2168db5789854d705dacd0b699
Christopher <Christopher@Chris>
2014-12-21 23:44:47 +0100

    Added SaveFileHandler

---
+++ Landung/src/oot/landung/filemanager/SaveFileHandler.java
@@ -0,0 +1,27 @@
+package oot.landung.filemanager;
+
+import java.io.IOException;
+
+import oot.landung.game.save.Save;
+
+public class SaveFileHandler {
+	private static final String file = "saves.ser";
+	
+	public static void resetSaves(){
+		FileHandler.deleteFile(file);
+	}
+
+	public static void saveGame(Save h) throws IOException, ClassNotFoundException{
+		FileHandler.writeSerializableToFile(h, file);
+		System.out.println("written");
+	}
+	
+	public static Save loadHighscores() throws ClassNotFoundException, IOException{
+		if(!FileHandler.fileExists(file)){
+			return new Save();
+		}
+		Save h = FileHandler.readSerializableFromFile(file, new Save());
+		return h;
+	}
+	
+}
